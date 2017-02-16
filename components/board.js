96184314916573a6c99fbb0de7c0637aa51738e8
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-02-15 18:18:05 -0800

    finishing props and state and adding comments

--- components/board.js
+++ components/board.js
@@ -3,13 +3,12 @@ import React from 'react';
 import List from './list';
 
 export default function Board(props) {
-    // return the fol
+    // return the following
     return (
         <div className="Board">
             <List title="first list" />
             <List title="2nd list" />
             <List title="3rd list" />
-
         </div>
     );
 }
