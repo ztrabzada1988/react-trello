26de62c47d19a8be446d6fefd0de87dc1c9ef151
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-02-15 19:05:33 -0800

    adding stateful components

--- components/board.js
+++ components/board.js
@@ -3,7 +3,7 @@ import React from 'react';
 import List from './list';
 
 export default function Board(props) {
-    // return the following lists to board (which is the main parent)
+    // return the following listss to board (which is the main parent)
     return (
         <div className="Board">
             <List title="first list" />
