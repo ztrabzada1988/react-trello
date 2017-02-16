e8bf1f5f4d42d9a9d0b8c7ec08d9c4f6e3e431fb
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-02-15 19:04:44 -0800

    adding stateful components

--- components/list.js
+++ components/list.js
@@ -2,14 +2,39 @@ import React from 'react';
 
 import Card from './card';
 
-export default function List(props) {
+export default class List extends React.Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <div className="list">
-            <div className="title">{props.title}</div>
-            <Card text="I am card 1" />
-            <Card text="I am card 2" />
-            <Card text="I am card 3" />
-        </div>
-    );
+        this.onAddInputChanged = this.onAddInputChanged.bind(this);
+        this.onAddSubmit = this.onAddSubmit.bind(this);
+    }
+
+
+    onAddInputChanged(event) {
+        event.preventDefault();
+        onAddSubmit(props);
+    }
+
+    onAddSubmit(event) {
+        event.preventDefault();
+        console.log("hello am i logged");
+    }
+
+
+    render() {
+        return (
+            <div className="list">
+                <div className="title">{this.props.title}</div>
+                <Card text="I am card 1" />
+                <Card text="I am card 2" />
+                <Card text="I am card 3" />
+
+                <form>
+                    <input onChange={this.onAddInputChanged} type="text" placeholder="Add a Card here"></input>
+                    <button type="button" onSubmit={this.onAddSubmit}>Submit</button>
+                </form>
+            </div>
+        );
+    }
 }
